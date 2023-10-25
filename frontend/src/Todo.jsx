dcd4a57827b64536358be64205a0d66b02b79981
code:babu <sbrbabu123@gmail.com>
2023-10-25 14:52:51 +0530

    Input is passing to View

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
-import "./Todo.css"
-import ViewTodo from './ViewTodo';
+import ViewTodo from './ViewTodo'; // ViewTodo on same page 
+import 'bootstrap/dist/css/bootstrap.css';  // Using bootstrap for styling
+
+// Todo function
 function Todo() {
 
+    // Required State variables
     const [event,setEvent] = useState("");
     const [detail,setDetail] = useState("");
     const [completed,setCompleted] = useState(false);
-    const [data,setData] = useState(null);
+    const [data,setData] = useState([]);
     const [isSubmit,setIsSubmit] = useState(false);
 
     
@@ -14,32 +17,48 @@ function Todo() {
     // Function to handle submit
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // New event => type - object
         const todoEvent = {event, detail, completed}
-        setData({...data, todoEvent});
+
+        // Adding new event on array
+        setData([...data, todoEvent]);
+
+        // Resetting the inputfields
+        setEvent(() => "");
+        setDetail(() => "");
+        setCompleted(() => false);
+        // Changing statevariable for 
         setIsSubmit(true);
+        
         console.log("Todo Event : ",todoEvent);
     }
+
 return (
     <>
-    
+    <div className="container" style ={ {"border" : "2px solid black"}}>
     <div className='todo-form'>
         
-        <input type='text' name='event' placeholder='Event' onChange={ (e) => {
+        <input class="input-group input-group-lg p-2 mt-4" type='text' value={event} name='event' placeholder='Event' onChange={ (e) => {
             let temp = e.target.value;
             setEvent(temp);
         }}/>
         <br/><br/>
-        <input type='text' name='detail' placeholder='Description' onChange={ (e) => {
+        <input class="input-group input-group-lg p-2" type='text' value={detail} name='detail' placeholder='Description' required onChange={ (e) => {
             let temp = e.target.value;
             setDetail(temp);
         }}/>
         <br/><br/>
-        <input type='checkbox' checked = {completed} onChange={ () => { setCompleted(!completed) }} />
+        <div className="form-check m-3">
+        <input className ="form-check-input" type='checkbox' checked = {completed} required  onChange={ () => { setCompleted(!completed) }} />
+        <label className ="form-check-label">Completed</label>
+        </div>
+        
 
-        <button onClick={handleSubmit} >Create</button>
+        <button class="btn btn-success mb-5" onClick={handleSubmit} >Create Task</button>
         
         {
-  isSubmit && data ? (
+  data && isSubmit  ? (
     <ViewTodo data={data} />
   ) : (
     isSubmit && <ViewTodo />
@@ -48,6 +67,7 @@ return (
 
         
         
+    </div>
     </div>
     </>
   )
--- frontend/src/ViewTodo.jsx
+++ frontend/src/ViewTodo.jsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for the upcoming test",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    description: "Go to the library to return Sammy's books",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write an article on how to use Django with React",
+    completed: false,
+  },
+];
 function ViewTodo ({data})  {
+
+  // Navbar tabs
+  const [viewCompleted, setViewCompleted] = useState(false);
+
+  // Checking Data
   console.log("View Data : ",data);
+
+  // Change navbartabs
+  const displayCompleted = (status) => {
+    setViewCompleted(status);
+  };
+
+  // Filtering the items
+  const renderItems = () =>{
+    if(data != null){
+      const res = data.filter((item) => item.completed === viewCompleted);
+    console.log("res : ",res);
+
+    return res.map((item) => (
+      <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
+        <span title={item.detail}>{item.event}</span>
+        <span>
+        <button className="btn btn-warning mr-2">Edit</button>
+        <button className="btn btn-danger mr-2">Delete</button>
+        </span>
+      </li>
+    ))
+    }
+    
+
+  }
   return (
+    <>
     <div>ViewTodo</div>
+    <div className="nav nav-tabs">
+    <span className={viewCompleted ? "nav-link active" : "nav-link"} onClick={() => displayCompleted(true)}>Completed</span>
+    <span className={viewCompleted ? "nav-link" : "nav-link active"} onClick={() => displayCompleted(false)}>To be Completed</span>
+    </div>
+    <ul className="list-group list-group-flush">
+    {renderItems()}
+    </ul>
+    </>
   )
 }
 
